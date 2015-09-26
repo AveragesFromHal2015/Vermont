41a1b901d94f3a6f2a2c629f59d161e8c54f3cc6
kazunari hirosawa <kazunario456@gmail.com>
2015-09-26 18:29:23 +0900

    Cartクラスの変更

--- src/jp/ac/hal/Beans/Cart.java
+++ src/jp/ac/hal/Beans/Cart.java
@@ -1,5 +1,35 @@
 package jp.ac.hal.Beans;
 
-public class Cart {
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Cart{
+	
+	private ArrayList<Product> product;
+	private Date date;
+	
+	public Cart(ArrayList<Product> product) {
+		this.product = product;
+		this.date = new Date();
+	}
+	
+	public void addCart(Product...products) {
+		for (int i = 0; i < products.length; i++) {
+			product.add(products[i]);
+		}
+	}
+	
+	//cartから消す処理
+	
+	public ArrayList<Product> getAllCart() {
+		return product;
+	}
+	
+	//クッキーにセット
+	public void setCookie() {
+		
+	}
+	
+	
 }
