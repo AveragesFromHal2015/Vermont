f6b7e7dc40181aba5d837e9aaad30ee6bb77f73a
Kazunari Hirosawa <Kazunario456@gmail.com>
2015-09-27 01:52:23 +0900

    ログイン処理追加

--- src/jp/ac/hal/Servlet/LoginServlet.java
+++ src/jp/ac/hal/Servlet/LoginServlet.java
@@ -1,11 +1,17 @@
 package jp.ac.hal.Servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.ac.hal.Dao.Dao;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,27 +19,138 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+	
+	private String errURL = "error.jsp";
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//変数の宣言
+		String mailAddress = "";
+		String pass = "";
+		String url = "";
+		String message = "";
+		boolean errFlg = false;
+
+		//データ・アクセスクラスを作る
+		Dao d ;	
+
+//		try{
+//			
+//			mailAddress = request.getParameter("mailAddress");
+//			pass = request.getParameter("password");
+//			
+//			if((mailAddress != null && !mailAddress.isEmpty()) && (pass != null && !pass.isEmpty())){
+//
+//				//ログインの結果のジャンプ
+//				if (d.loginSQL(mailAddress, pass) == true){
+//					HttpSession session = request.getSession(true);
+//					
+//					//セッションの削除
+//					session.removeAttribute("mailAddress");
+//					session.removeAttribute("id");
+//					session.removeAttribute("name");
+//					
+//					//セッションの保存
+//					session.setAttribute("mailAddress", mailAddress);
+//					session.setAttribute("name", d.getUserName());
+//					session.setAttribute("id", d.getUserId());
+//					
+//					if(request.getParameter("option") != null){
+//						
+//						//cookieの保存
+//						Cookie optionCookie = new Cookie("option","0");
+//						Cookie userCookie = new Cookie("mailAddress",mailAddress);
+//						Cookie passCookie = new Cookie("pass",pass);
+//						
+//						//クッキーの有効時間設定(有効期間1週間)
+//						optionCookie.setMaxAge(7*24*60*60);//optionCookieオブジェクトの有効時間設定
+//						userCookie.setMaxAge(7*24*60*60);//userCookieオブジェクトの有効時間設定
+//						passCookie.setMaxAge(7*24*60*60);//passCookieオブジェクトの有効時間設定
+//						
+//						//クッキーをクライアントに作成する
+//						response.addCookie(optionCookie);//optionCookieオブジェクトのクッキーをクライアントに作成
+//						response.addCookie(userCookie);//userCookieオブジェクトのクッキーをクライアントに作成
+//						response.addCookie(passCookie);//passCookieオブジェクトのクッキーをクライアントに作成
+//
+//					}else{
+//						//cookieの削除
+//						Cookie optionCookie = new Cookie("option","");
+//						Cookie userCookie = new Cookie("user","");
+//						Cookie passCookie = new Cookie("pass","");
+//						
+//						//クッキーの有効時間設定(有効期間1週間)
+//						optionCookie.setMaxAge(0);//optionCookieオブジェクトの有効時間設定
+//						userCookie.setMaxAge(0);//userCookieオブジェクトの有効時間設定
+//						passCookie.setMaxAge(0);//passCookieオブジェクトの有効時間設定
+//						
+//						//クッキーをクライアントに作成する
+//						response.addCookie(optionCookie);//optionCookieオブジェクトのクッキーをクライアントに作成
+//						response.addCookie(userCookie);//userCookieオブジェクトのクッキーをクライアントに作成
+//						response.addCookie(passCookie);//passCookieオブジェクトのクッキーをクライアントに作成
+//					}
+//
+//					
+//					//ログイン処理
+//					url = "user.jsp";
+//				}else {
+//					//転送するMessage
+//					message = "メールアドレス、もしくはパスワードが間違っています。";
+//					//エラー処理
+//					errFlg = true;
+//					url = "error.jsp";
+//				}
+//			}else{
+//				//エラーあり
+//				errFlg = true;
+//
+//				if(mailAddress == null || mailAddress.isEmpty()){
+//					message = "メールアドレスを入力してください。";
+//				}
+//
+//				if(pass == null || pass.isEmpty()){
+//					message = message + "パスワードを入力してください。";
+//				}
+//				
+//				//エラー処理
+//				url = errURL;		
+//			}
+//
+//			
+//		}catch(SQLException e){
+//
+//			//SQLエラーの場合
+//			errFlg = true;
+//			url = errURL;
+//			
+//			//転送するMessage
+//			message = "データベースエラーが発生しました。"+ e;
+//			
+//		}catch(Exception e){
+//
+//			//エラーの場合
+//			errFlg = true;
+//			url = errURL;
+//
+//			//転送するMessage
+//			message = "エラーが発生しました。"+ e;
+//
+//		}finally{
+//
+//			if(errFlg == false){//エラー無し
+//				
+//				response.sendRedirect(url);
+//
+//			}else{//エラーあり
+//				//Messageをセット
+//				request.setAttribute("errorMessage",message);
+//				//ディスパッチオブジェクト作成
+//				RequestDispatcher rd = request.getRequestDispatcher(url);
+//				//リクエストを転送
+//				rd.forward(request,response);
+//			}
+//		}
 	}
 
 }
