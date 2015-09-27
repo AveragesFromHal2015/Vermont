5044d489ff32330f248f41209b356b62b88ed88f
Kazunari Hirosawa <Kazunario456@gmail.com>
2015-09-27 21:14:53 +0900

    ログアウト処理追加

--- src/jp/ac/hal/Cmn/CmnVal.java
+++ src/jp/ac/hal/Cmn/CmnVal.java
@@ -1,5 +1,9 @@
 package jp.ac.hal.Cmn;
 
+import java.util.HashMap;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
 /**
  * 共通の定数
  * @author kazu
@@ -10,20 +14,32 @@ public class CmnVal {
 	//エラー時のジャンプ先URL
 	public static final String errURL = "error.jsp";
 	public static final String LoginURL = "login.jsp";
+	public static final String TopURL = "index.jsp";
+	
+	//エラーステータスコード
+	/*
+	 * 800 ログイン時エラーメッセージ 
+	 * 810 アクセスエラー
+	 */
+	public static final int errCode = 800;
+	public static final int errCodeLoginFalse = 801;
+	public static final int errCodeLoginNullMail = 802;
+	public static final int errCodeLoginNullId = 803;
+	public static final int errCodeLoginNullPass = 804;
+	public static final int errCodeAccsessFalse = 810;
 
 	//エラーメッセージ
 	public static final String errMsgLoginFalse = "メールアドレス、もしくはパスワードが間違っています。";
 	public static final String errMsgLoginNullMail = "メールアドレスを入力してください。";
 	public static final String errMsgLoginNullId = "IDを入力してください。";
 	public static final String errMsgLoginNullPass = "パスワードを入力してください。";
-
-	public static final String attributeMass = "message";
+	public static final String errMsgAccsessFalse = "不正なアクセスです。";
 	
+	public static final String attributeMass = "message";	
 	//間違えないために定数化
 	public static final String FormValPassword = "password";
 	public static final String FormValMail = "mailAddress";
 	public static final String FormValId = "user_id";
 	public static final String FormValName = "name";
-	
-	
+		
 }
